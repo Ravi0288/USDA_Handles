5d3b621867b85e68e0c76da8b7121c721e6233b6
pk15pi <76875206+pk15pi@users.noreply.github.com>
2024-11-14 17:00:47 +0530

    dev settings added

--- handles/settings.py
+++ handles/settings.py
@@ -16,20 +16,16 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-_9)o=-a0(%_6ijny@!orgnjw%ks^=u@hw-jh#e8(ty_5m-g3yq'
+# SECRET_KEY = get_env_variable('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
-
 INSTALLED_APPS = [
     'corsheaders',
     'django.contrib.admin',
